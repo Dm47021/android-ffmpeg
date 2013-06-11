f55ef45017e8459466c1695a8f2f091c866cb88c
Rob Beane <robbeane@gmail.com>
2013-06-11 15:35:23 -0400

    update ffmpeg to latest release/1.2 branch

--- configure_ffmpeg.sh
+++ configure_ffmpeg.sh
@@ -11,12 +11,11 @@ fi
 # so just always try to apply it, and ignore it if it fails. Works fine unless
 # the files being patched have changed, in which cause a partial application
 # could happen unnoticed.
-patch -N -p1 --reject-file=- < redact-plugins.patch
-patch -N -p1 --reject-file=- < arm-asm-fix.patch
-patch -d ffmpeg -N -p1 --reject-file=- < \
-    ARM_generate_position_independent_code_to_access_data_symbols.patch
-patch -d ffmpeg -N -p1 --reject-file=- < \
-    ARM_intmath_use_native-size_return_types_for_clipping_functions.patch
+#patch -N -p1 --reject-file=- < redact-plugins.patch
+#patch -d ffmpeg -N -p1 --reject-file=- < \
+#    ARM_generate_position_independent_code_to_access_data_symbols.patch
+#patch -d ffmpeg -N -p1 --reject-file=- < \
+#    ARM_intmath_use_native-size_return_types_for_clipping_functions.patch
 patch -d ffmpeg -N -p1 --reject-file=- < \
     enable-fake-pkg-config.patch
 
@@ -32,6 +31,7 @@ $DEBUG_FLAG \
 --enable-pic \
 --disable-shared \
 --enable-static \
+--enable-thumb \
 --cross-prefix=$NDK_TOOLCHAIN_BASE/bin/$NDK_ABI-linux-androideabi- \
 --sysroot="$NDK_SYSROOT" \
 --extra-cflags="-I../x264 -mfloat-abi=softfp -mfpu=neon" \
